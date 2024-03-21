a4015beb51b9477025bdef1ec3a045f62336dc81
Marko Kask <kaskmarko@gmail.com>
2024-03-21 15:01:13 +0200

    Add language item edit

--- client/src/components/cv/CVLanguageItem.jsx
+++ client/src/components/cv/CVLanguageItem.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./CVLanguageItem.css";
+import CVLangItemForm from "./forms/CVLangItemForm";
 
 function CVLanguageItem({id, cvId, language, getAllLanguages}){
 
     const[onHover, setOnHover] = useState(false);
+    const[wasClicked, setWasClicked] = useState(false);
 
     function handleMouseHover(){
         setOnHover(!onHover);
+    }
+
+    function toggleForm(){
+       setWasClicked(!wasClicked);
     }
 
     async function deleteLanguageItem(){
@@ -20,11 +26,11 @@ function CVLanguageItem({id, cvId, language, getAllLanguages}){
         }
     }
 
-    //<span class="text">English&nbsp;&nbsp;B2</span>
     return(
-        <div className="contactInfo languages" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}>
+        wasClicked ? <CVLangItemForm id={id} cvId={cvId} getAllLanguages={getAllLanguages}  language={language} toggleForm={toggleForm}/> :
+        <div className="contactInfo languages" onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover} onClick={toggleForm}>
             <div className="language">
-                <p className="text lang-text">English B2</p>
+                <p className="text lang-text">{language}</p>
                 {
                     onHover && <i className="lang-delete fa fa-trash-o" aria-hidden="true" onClick={deleteLanguageItem}></i>
                 }      
---
+++ client/src/components/cv/forms/CVLangItemForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './CVLangItemForm.css';
+
+function CVLangItemForm({id, cvId, getAllLanguages, toggleForm, language}){
+
+    const[lang, setLang] = useState({
+        language : language
+    });
+    
+    function handleChange(event){
+        const{name, value} = event.target;
+        setLang(prevValue => {
+            return{
+                ...prevValue,
+                [name] : value
+            }
+        });
+    }
+
+    async function editLangItem(){
+        try{
+            const response = await fetch(`${process.env.REACT_APP_SERVERURL}/cv/languages/${id}`, {
+                method: "PUT",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    language: lang.language
+                })
+            });
+            getAllLanguages(cvId);
+        }catch(err){
+            console.error(err);
+        }
+        toggleForm(); 
+    }
+
+    return(
+        <div className="contactInfo languages">
+        <div className="language language-item-form ">
+            <p className="text lang-text"><input type="text" name="language" value={lang.language} onChange={handleChange}></input></p> 
+            <span className="contactInfo-save-form"><i onClick={editLangItem}  className="fa fa-check-circle-o fa-lg edit-lang-item" aria-hidden="true"></i></span>
+            <h2 className="education-cancel-edit" onClick={toggleForm}>x</h2>  
+        </div>
+    </div>
+    )
+}
+
+export default CVLangItemForm;
